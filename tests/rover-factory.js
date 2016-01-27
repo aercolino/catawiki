64565390fb28d8367ee256af56685f7499f8dd52
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2016-01-27 03:04:57 +0100

    cleaned up code

--- tests/rover-factory.js
+++ tests/rover-factory.js
@@ -2,6 +2,10 @@ var assert = require('assert');
 var _ = require('lodash');
 var Rover = require('../src/rover-factory');
 
+var my = {
+    config: {xMax: 5, yMax:5, faces: 'NESW'},
+    line_number: 3
+};
 
 describe('Rover Factory', function() {
 
@@ -9,7 +13,7 @@ describe('Rover Factory', function() {
 
         it('should have the start position before any movement', function() {
 
-            var rover = Rover({x: 1, y: 1, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+            var rover = Rover({x: 1, y: 1, facing: 'N'}, my.config, my.lineNumber);
             assert.equal('1 1 N', rover.toString());
 
         });
@@ -18,7 +22,7 @@ describe('Rover Factory', function() {
         it('should throw an exception when the start is not inside the grid', function() {
 
             function bubble_exception() {
-                Rover({x: 6, y: 6, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                Rover({x: 6, y: 6, facing: 'N'}, my.config, my.lineNumber);
             }
             assert.throws(bubble_exception, 'Error');
             
@@ -34,7 +38,7 @@ describe('Rover Factory', function() {
 
             it('should face East if it started facing North', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'N'}, my.config, my.lineNumber);
                 rover.move('R');
                 assert.equal('1 1 E', rover.toString());
 
@@ -43,7 +47,7 @@ describe('Rover Factory', function() {
 
             it('should face South if it started facing East', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'E'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'E'}, my.config, my.lineNumber);
                 rover.move('R');
                 assert.equal('1 1 S', rover.toString());
 
@@ -52,7 +56,7 @@ describe('Rover Factory', function() {
 
             it('should face West if it started facing South', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'S'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'S'}, my.config, my.lineNumber);
                 rover.move('R');
                 assert.equal('1 1 W', rover.toString());
 
@@ -61,7 +65,7 @@ describe('Rover Factory', function() {
 
             it('should face North if it started facing WestLeftLe', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'W'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'W'}, my.config, my.lineNumber);
                 rover.move('R');
                 assert.equal('1 1 N', rover.toString());
 
@@ -74,7 +78,7 @@ describe('Rover Factory', function() {
 
             it('should face East if it started facing South', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'S'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'S'}, my.config, my.lineNumber);
                 rover.move('L');
                 assert.equal('1 1 E', rover.toString());
 
@@ -83,7 +87,7 @@ describe('Rover Factory', function() {
 
             it('should face South if it started facing West', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'W'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'W'}, my.config, my.lineNumber);
                 rover.move('L');
                 assert.equal('1 1 S', rover.toString());
 
@@ -92,7 +96,7 @@ describe('Rover Factory', function() {
 
             it('should face West if it started facing North', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'N'}, my.config, my.lineNumber);
                 rover.move('L');
                 assert.equal('1 1 W', rover.toString());
 
@@ -101,7 +105,7 @@ describe('Rover Factory', function() {
 
             it('should face North if it started facing East', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'E'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'E'}, my.config, my.lineNumber);
                 rover.move('L');
                 assert.equal('1 1 N', rover.toString());
 
@@ -114,7 +118,7 @@ describe('Rover Factory', function() {
 
             it('should increase y if it started facing North', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'N'}, my.config, my.lineNumber);
                 rover.move('M');
                 assert.equal('1 2 N', rover.toString());
 
@@ -123,7 +127,7 @@ describe('Rover Factory', function() {
 
             it('should decrease y if it started facing South', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'S'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'S'}, my.config, my.lineNumber);
                 rover.move('M');
                 assert.equal('1 0 S', rover.toString());
 
@@ -132,7 +136,7 @@ describe('Rover Factory', function() {
 
             it('should increase x if it started facing East', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'E'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'E'}, my.config, my.lineNumber);
                 rover.move('M');
                 assert.equal('2 1 E', rover.toString());
 
@@ -141,7 +145,7 @@ describe('Rover Factory', function() {
 
             it('should decrease x if it started facing West', function() {
 
-                var rover = Rover({x: 1, y: 1, facing: 'W'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                var rover = Rover({x: 1, y: 1, facing: 'W'}, my.config, my.lineNumber);
                 rover.move('M');
                 assert.equal('0 1 W', rover.toString());
 
@@ -155,7 +159,7 @@ describe('Rover Factory', function() {
             it('should not go over the yMax', function() {
 
                 function bubble_exception() {
-                    var rover = Rover({x: 5, y: 5, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                    var rover = Rover({x: 5, y: 5, facing: 'N'}, my.config, my.lineNumber);
                     rover.move('M');
                 }
                 assert.throws(bubble_exception, 'Error');
@@ -165,7 +169,7 @@ describe('Rover Factory', function() {
             it('should not go below the yMin', function() {
 
                 function bubble_exception() {
-                    var rover = Rover({x: 0, y: 0, facing: 'S'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                    var rover = Rover({x: 0, y: 0, facing: 'S'}, my.config, my.lineNumber);
                     rover.move('M');
                 }
                 assert.throws(bubble_exception, 'Error');
@@ -175,7 +179,7 @@ describe('Rover Factory', function() {
             it('should not go after the xMax', function() {
 
                 function bubble_exception() {
-                    var rover = Rover({x: 5, y: 5, facing: 'E'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                    var rover = Rover({x: 5, y: 5, facing: 'E'}, my.config, my.lineNumber);
                     rover.move('M');
                 }
                 assert.throws(bubble_exception, 'Error');
@@ -185,7 +189,7 @@ describe('Rover Factory', function() {
             it('should not go before the xMin', function() {
 
                 function bubble_exception() {
-                    var rover = Rover({x: 0, y: 0, facing: 'W'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                    var rover = Rover({x: 0, y: 0, facing: 'W'}, my.config, my.lineNumber);
                     rover.move('M');
                 }
                 assert.throws(bubble_exception, 'Error');
