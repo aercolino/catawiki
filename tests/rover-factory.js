2c003467e34b5935418d6e6ee669aca5cfba53ac
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2016-01-27 02:36:23 +0100

    testing rover-factory

--- tests/rover-factory.js
+++ tests/rover-factory.js
@@ -26,4 +26,129 @@ describe('Rover Factory', function() {
 
     });
 
+
+
+    describe('a rover turns and moves', function() {
+
+        describe('a rover turns Right', function() {
+
+            it('should face East if it started facing North', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('R');
+                assert.equal('1 1 E', rover.toString());
+
+            });
+
+
+            it('should face South if it started facing East', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'E'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('R');
+                assert.equal('1 1 S', rover.toString());
+
+            });
+
+
+            it('should face West if it started facing South', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'S'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('R');
+                assert.equal('1 1 W', rover.toString());
+
+            });
+
+
+            it('should face North if it started facing WestLeftLe', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'W'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('R');
+                assert.equal('1 1 N', rover.toString());
+
+            });
+
+        });
+
+
+        describe('a rover turns Left', function() {
+
+            it('should face East if it started facing South', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'S'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('L');
+                assert.equal('1 1 E', rover.toString());
+
+            });
+
+
+            it('should face South if it started facing West', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'W'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('L');
+                assert.equal('1 1 S', rover.toString());
+
+            });
+
+
+            it('should face West if it started facing North', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('L');
+                assert.equal('1 1 W', rover.toString());
+
+            });
+
+
+            it('should face North if it started facing East', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'E'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('L');
+                assert.equal('1 1 N', rover.toString());
+
+            });
+
+        });
+
+
+        describe('a rover moves', function() {
+
+            it('should increase y if it started facing North', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'N'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('M');
+                assert.equal('1 2 N', rover.toString());
+
+            });
+
+
+            it('should decrease y if it started facing South', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'S'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('M');
+                assert.equal('1 0 S', rover.toString());
+
+            });
+
+
+            it('should increase x if it started facing East', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'E'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('M');
+                assert.equal('2 1 E', rover.toString());
+
+            });
+
+
+            it('should decrease x if it started facing West', function() {
+
+                var rover = Rover({x: 1, y: 1, facing: 'W'}, {xMax: 5, yMax:5, faces: 'NESW'}, 3);
+                rover.move('M');
+                assert.equal('0 1 W', rover.toString());
+
+            });
+
+        });
+
+    });
+
 });
